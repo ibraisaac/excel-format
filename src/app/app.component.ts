f818f246e3d9e958230ea0ad075390f9b8b771f1
Isaac Rodrigues Oliveira <isaac.oliveira@accelera.com.br>
2024-06-14 15:29:28 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule, MatButtonModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'excel-format';
+  arquivoSelecionado = 'Nenhum arquivo selecionado';
+
+  aoSelecionarArquivo(event: any): void {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.arquivoSelecionado = file.name;
+    }
+  }
+
+  selecionarArquivo(): void {
+    document.getElementById('inputFile')?.click();
+  }
+
+  formatarArquivo(): void {
+
+  }
 }
